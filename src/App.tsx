49234105e0c939909dab23bacef8877ea11e8855
justjazz903 <justjazz903@gmail.com>
2024-04-10 19:13:06 +0800

    Wed, Apr 10, 2024  7:13:06 PM

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="justify-center items-center w-64 h-64 bg-slate-500 hover:bg-pink-500">
+      
+    </div>
+  );
+}
+
+export default App;
