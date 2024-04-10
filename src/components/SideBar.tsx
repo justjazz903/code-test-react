fb4efa3c5360dcf79a7d0bccee533869cff5cd10
justjazz903 <justjazz903@gmail.com>
2024-04-11 04:27:26 +0800

    Thu Apr 11 04:27:26 CST 2024

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -30,17 +30,17 @@ export default function SideBar({set_data, set_err_msg}: {set_data: Function, se
   const [city, set_city] = useState("")
   const [loading, set_loading] = useState(false)
 
-  const on_search = (e: React.FormEvent<HTMLFormElement>) => {
+  const on_search = (e: React.FormEvent<HTMLFormElement>, arg_city: string) => {
     e.preventDefault();
     set_loading(true)
     fetch(
-      'http://127.0.0.1:5000/forecast',
+      'https://code-test-flask-dot-nextapp-412807.df.r.appspot.com/forecast',
       {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({city}),
+        body: JSON.stringify({city: arg_city}),
       }
     ).then((res) => {
       if (res.ok) {
@@ -70,7 +70,7 @@ export default function SideBar({set_data, set_err_msg}: {set_data: Function, se
       <div className="flex w-full items-center justify-center py-8" onClick={()=>set_city("")}>
         <TiWeatherPartlySunny className="w-32 h-32 fill-blue-400"/>
       </div>
-      <form method="post" onSubmit={on_search}>
+      <form method="post" onSubmit={(e: any)=>{on_search(e, city)}}>
         <div className="flex flex-row px-4 py-2">
           <input
             type="text"
@@ -91,15 +91,18 @@ export default function SideBar({set_data, set_err_msg}: {set_data: Function, se
         </div>
         <div className="grid grid-cols-2 mt-6 mx-2">
           {
-            popular_cities.map((city, index) => (
+            popular_cities.map((popular_city, index) => (
               <div key={index} className="px-4 py-2">
                 <button
+                  type="button"
                   onClick={(e: any) => {
-                    set_city(city)
+                    set_city(popular_city)
+                    on_search(e, popular_city)
                   }}
+                  disabled={loading}
                   className="text-lg hover:border-b hover:border-blue-500 hover:text-blue-500 focus:outline-none transition-all duration-200"
                 >
-                  {city}
+                  {popular_city}
                 </button>
               </div>
             ))
